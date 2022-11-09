fcf5152eea4ede111e122e129f6f2a4cfbaae6e1
hehe <2952413857@qq.com>
2022-11-09 16:42:20 +0800

    hot-fix first commit

--- src/main/java/com/doudou/git/GitTest.java
+++ src/main/java/com/doudou/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello,git");
         System.out.println("hi,github");
         System.out.println("hi,doudou");
+        System.out.println("hello,doudou");
     }
 }
